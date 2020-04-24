e87efbdbd31def76f01780988bc3307e61c6fc3d
Hasini-Devindi <it17026208@my.sliit.lk>
2020-04-24 20:55:52 +0530

    IT17026208

--- app/src/main/java/com/expensemanager/personalexpensemanager/splash.java
+++ app/src/main/java/com/expensemanager/personalexpensemanager/splash.java
@@ -1,22 +1,36 @@
 package com.expensemanager.personalexpensemanager;
-
-
+//
+//
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+//
+//
+ public  class splash extends AppCompatActivity {
+//
+//    //Button start_btn;
+      ImageButton fab;
 
-
-
-public abstract class splash extends Activity {
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+//
+           fab = findViewById(R.id.fab);
+//
+           fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(splash.this, MainActivity.class));
 
-
-
-        }}
-
-
+            }
+             });
+//
+//
+     }
+//
+}
