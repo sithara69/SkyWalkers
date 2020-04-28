e1d63fb77cfc1389f016369b176dedb196400f1a
IT16003606 <it16003606@my.sliit.lk>
2020-04-28 11:42:21 +0530

    IT16003606

--- app/src/main/java/com/expensemanager/personalexpensemanager/splash.java
+++ app/src/main/java/com/expensemanager/personalexpensemanager/splash.java
@@ -1,14 +1,28 @@
 package com.expensemanager.personalexpensemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class splash extends Activity {
+public  class splash extends AppCompatActivity {
+    ImageButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-    }
 
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(splash.this, MainActivity.class));
+
+            }
+        });
+
+    }
 }
